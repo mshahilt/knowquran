f6b4f54f5a4a9e210d40407f80754588c4ce7876
mshahilt <mshahilt3@gmail.com>
2025-01-28 13:56:12 +0530

    first commit

---
+++ server/routes/costumer.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customerController'); // Ensure path is correct
+
+router.get('/');
